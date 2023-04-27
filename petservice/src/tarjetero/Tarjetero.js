5f223c525240aab52b4c4e15f92cf141c81368d0
rgasali <rgasali@gmail.com>
2023-04-27 19:38:51 -0300

    se creo tarjetaBlog y se cambiaron colores de tarjeta

--- petservice/src/tarjetero/Tarjetero.js
+++ petservice/src/tarjetero/Tarjetero.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Tarjeta from '../tarjeta/Tarjeta'
 import "./tarjetero.css"
+import TarjetaBlog from '../tarjetaBlog/TarjetaBlog'
 
 const Tarjetero = () => {
   return (
     <div className='tarjetero'>
+        <TarjetaBlog/>
+        <TarjetaBlog/>
+        <TarjetaBlog/>
+        <TarjetaBlog/>
         <Tarjeta/>
         <Tarjeta/>
         <Tarjeta/>
