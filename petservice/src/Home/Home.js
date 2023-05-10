a36e6345f34ae3750bba89278eff849af961895c
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-05-10 18:26:00 -0300

    se creó el Footer y ventana modal para sección Preguntas Frecuentes

--- petservice/src/Home/Home.js
+++ petservice/src/Home/Home.js
@@ -3,20 +3,22 @@ import Navbar from './Navbar';
 import './Home.css'
 import logo from '../assets/logo.png'
 import Portada from './Portada';
+import Footer from './Footer';
 // import Tarjetero from '../tarjetero/Tarjetero';
 
 function Home() {
   return (
     <div>
-  <header>
-        <img src={logo} alt="Logo" width="200" height="200" style={{ marginRight: 'auto' }} />
+      <header>
+        <img src={logo} alt="Logo" width="170" height="170" style={{ marginRight: 'auto' }} />
         
        <Navbar />
-  <div className="login-link">
+      <div className="login-link">
        <button className="btn btn-outline-primary me-2" type="button"> Iniciar sesión</button>
-  </div>
-</header>
-<Portada/>
+      </div>
+    </header>
+      <Portada/>
+      <Footer/>
 {/* <Tarjetero/> */}
     </div>
   );
