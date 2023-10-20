9f29f223a9b22ddfc905954b9932c7fdf8f54fb3
florbelmonte <mariaflorenciabelmonterojas@gmail.com>
2023-10-19 22:10:33 -0300

    se instaló axios y se probo en tarjetero traer datos del back

--- petservice/src/tarjetero/Tarjetero.js
+++ petservice/src/tarjetero/Tarjetero.js
@@ -6,10 +6,24 @@ import CrearTarjeta from '../tarjeta/crearTarjeta/CrearTarjeta'
 import { useContext } from 'react'
 import { ServiciosContext } from '../context/ServiciosContext'
 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const Tarjetero = (servicioElejido) => {
 
   const contextServicios=useContext(ServiciosContext)
   
+  const [data, setData] = useState('');
+  
+  useEffect(() => {
+    axios.get('http://localhost:3000/tarjeta-servicio')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   //filtro de busqueda segun el servicio seleccionado (desde la barra de navegacion)
   const tarjetasFiltradasPorServicio=contextServicios.servicios.filter(tarjeta=>{return tarjeta.servicio===servicioElejido.servicioElejido});
